2abf514856f6969f304f89b2eb2da85b6cce6173
arianaa1m <ariana.munoz@ucb.edu.bo>
2024-08-23 11:39:07 -0400

    prender led

---
+++ Pulsador.ino
@@ -0,0 +1,18 @@
+#define LED 2
+#define PUL 8
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(PUL, INPUT);
+}
+
+void loop()
+{
+  if(digitalRead(PUL)){
+	digitalWrite(LED, 1);
+}
+  else{
+    digitalWrite(LED, 0);
+  } 
+}
